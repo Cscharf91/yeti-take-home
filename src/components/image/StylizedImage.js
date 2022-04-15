1e379810be53212a741eaaac16a034bf000b9365
Cory <cscharf91@gmail.com>
2022-04-15 12:23:54 -0700

    about section completed and made responsive

---
+++ src/components/image/StylizedImage.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const StylizedImage = ({ src, alt, shadowDirection, width }) => {
+  const getBoxShadow = (dir) => {
+    switch (dir) {
+      case "se":
+        return "rgb(235 234 235) 25px 30px";
+
+      default:
+        break;
+    }
+  };
+
+  return (
+    <img
+      className="img"
+      src={src}
+      alt={alt}
+      style={{ boxShadow: getBoxShadow(shadowDirection), width: width, zIndex: "-1" }}
+    />
+  );
+};
+
+export default StylizedImage;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,38 @@
-import React from 'react'
-import Header from '../components/header/Header'
+import React from "react";
+import Header from "../components/header/Header";
+import StylizedImage from "../components/image/StylizedImage";
+import img1 from "../assets/images/img1.jpg";
 
 const Home = () => {
   return (
     <div className="page-wrapper">
-      <Header />
+      <div className="page-container">
+        <Header />
+        <section>
+          <div className="flex-center width-100 height-100 about-sect">
+            <div className="flex-column">
+              <h2 className="sect-title align-start">About</h2>
+              <StylizedImage
+                src={img1}
+                shadowDirection="se"
+                width="max(60%, 650px)"
+              />
+            </div>
+            <div className="sm-textbox align-center min-width-250">
+              <p>
+                <strong>Andy Griffith turned us down.</strong>
+              </p>
+              <p>
+                Army had half a day. Bad news. Andy Griffith turned us down. He
+                didn't like his nice trailer. That's why you always leave a
+                note!
+              </p>
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
