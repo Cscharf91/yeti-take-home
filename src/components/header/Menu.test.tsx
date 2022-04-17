3a60d732679a95e8d659e9c45b585c788541f6a1
Cory <cscharf91@gmail.com>
2022-04-16 18:42:45 -0700

    changed wording for tests in Menu.test.tsx

--- src/components/header/Menu.test.tsx
+++ src/components/header/Menu.test.tsx
@@ -3,13 +3,13 @@ import { render, screen } from "@testing-library/react";
 import { click } from "@testing-library/user-event/dist/click";
 
 describe('Menu Component', () => { 
-  it('should render without menu open', () => {
+  it('should render with the show-menu toggled off', () => {
     render(<Menu />);
     const list = screen.getByRole('list')
     expect(list.classList.contains("show-menu")).toBeFalsy();
   });
 
-  it('should render the menu when clicked on', () => {
+  it('should toggle the show-menu class on when the menu is clicked on', () => {
     render(<Menu />);
     const menu = screen.getByTestId('menu');
     const list = screen.getByRole('list')
